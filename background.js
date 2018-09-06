44cfdc06dc9d70779040f6e863f8981b4b0553c4
Damian <42821489+PowerOfUniverse@users.noreply.github.com>
2018-09-06 03:28:23 +0200

    The problem with turning off aim has been fixed

--- background.js
+++ background.js
@@ -1,1 +1,233 @@
-var generateVaribaleName=function(){return "_"+ Math["random"]().toString(36)["substring"](7)};var variableNames={exports:"exports",canvas:"canvas",mouseMoveEvent:"mouseMoveEvent",keydown:"keydown",keyup:"keyup"};function patchAppCode(_0xa752x3){var _0xa752x4=[{name:"Export game scope",from:/([a-zA-Z]).scrollDelta=0,([a-zA-Z]).tmpInputs.push\(([a-zA-Z])\),/g,to:"window.cheat(L, y, v.getVars(), M, v, N),$1.scrollDelta=0,$2.tmpInputs.push($3),"},{name:"Exports exports scope",from:/!function\(([a-z])\){var ([a-z])={};function/g,to:"!function($1){var $2={};window[\""+ variableNames["exports"]+ "\"]=$2;function"},{name:"Mousemove",from:/;([a-z]).addEventListener\("mousemove"/g,to:";window[\""+ variableNames["mouseMoveEvent"]+ "\"]=$1;$1.addEventListener(\"mousemove\""},{name:"keydown",from:/,window.addEventListener\("keydown",function\(([a-zA-Z])\){/g,to:",window.addEventListener(\"keydown\",function($1){window.keydown($1),"},{name:"keyup",from:/,window.addEventListener\("keyup",function\(([a-zA-Z])\){/g,to:",window.addEventListener(\"keyup\",function($1){window.keyup($1),"},{name:"Functions & constants",from:/,this.canSee=function\(/,to:",this.getVars=function(){return {funcs: i,constants: a}},this.canSee=function("}];_0xa752x4["forEach"](function(_0xa752x5){if(_0xa752x5["from"]["test"](_0xa752x3)){_0xa752x3= _0xa752x3["replace"](_0xa752x5["from"],_0xa752x5["to"])}else {console["log"]("Err patching: "+ _0xa752x5["name"])}});var _0xa752x6=chrome["extension"]["getURL"]("init.js");return fetch(_0xa752x6)["then"]((_0xa752x8)=>_0xa752x8["text"]())["then"]((_0xa752x7)=>{return _0xa752x7+ _0xa752x3})}var codeInjector=(function(){var _0xa752xa=null;var _0xa752xb=false;function _0xa752xc(_0xa752x6,_0xa752xd,_0xa752xe){console["log"]("Executing xhr app request...");var _0xa752xf= new XMLHttpRequest();_0xa752xf["open"]("GET",_0xa752x6,true);_0xa752xf["send"]();_0xa752xf["onreadystatechange"]= function(){if(_0xa752xf["readyState"]!= 4){return};if(this["status"]!= 200){return _0xa752xe()};chrome["storage"]["local"]["set"]({"appCode":_0xa752xf["responseText"],"appVer":_0xa752x6["match"](/game.js/)[1]},function(){return _0xa752xd(_0xa752xf["responseText"])})}}var _0xa752x10=function(_0xa752x3){_0xa752xa= _0xa752x3};var _0xa752x11=function(_0xa752x3,_0xa752x12){patchAppCode(_0xa752x3)["then"](function(_0xa752x13){codeInjector["setAppCode"](_0xa752x13);_0xa752xb= false;codeInjector["tryToInjectCode"](_0xa752x12)})};var _0xa752x14=function(_0xa752x12,_0xa752x15){var _0xa752x16=JSON["stringify"]({code:_0xa752x15});var _0xa752x17="(function(){";_0xa752x17+= "var code = (";_0xa752x17+= _0xa752x16;_0xa752x17+= ").code;";_0xa752x17+= "var script = document.createElement('script');";_0xa752x17+= "script.innerHTML = code;";_0xa752x17+= "document.body.appendChild(script);";_0xa752x17+= "})()";try{chrome["tabs"]["executeScript"](_0xa752x12,{code:_0xa752x17})}catch(e){}};var _0xa752x18=function(_0xa752x12){if(_0xa752xa){_0xa752x14(_0xa752x12,_0xa752xa);_0xa752xa= null;return}};var _0xa752x19=function(_0xa752x1a,_0xa752x1b){if(_0xa752x1a["url"]["match"](/game/)){if(!_0xa752xb){_0xa752xb= true}else {return};chrome["storage"]["local"]["get"](["appCode"],function(_0xa752x3){if(_0xa752x3["appCode"]=== undefined){codeInjector["updateAppCode"](_0xa752x1a["url"],function(_0xa752x3){console["log"]("App code updated.");_0xa752x11(_0xa752x3,_0xa752x1b["id"])},function(){_0xa752xb= false;console["log"]("Err update game file. Page will be reloaded after 5 seconds...");setTimeout(function(){chrome["tabs"]["reload"](_0xa752x1b["id"],null,null)},5000)})}else {chrome["storage"]["local"]["get"](["appVer"],function(_0xa752x1c){if(_0xa752x1c["appVer"]!= _0xa752x1a["url"]["match"](/game.js/)[1]){codeInjector["updateAppCode"](_0xa752x1a["url"],function(_0xa752x3){console["log"]("App code updated.");_0xa752x11(_0xa752x3,_0xa752x1b["id"])},function(){_0xa752xb= false;console["log"]("Err update game file. Page will be reloaded after 5 seconds...");setTimeout(function(){chrome["tabs"]["reload"](_0xa752x1b["id"],null,null)},5000)})}else {_0xa752x11(_0xa752x3["appCode"],_0xa752x1b["id"])}})}})}};return {updateAppCode:_0xa752xc,setAppCode:_0xa752x10,tryToInjectCode:_0xa752x18,onRequest:_0xa752x19}})();var onBeforeRequestListener=function(_0xa752x1a){chrome["tabs"]["get"](_0xa752x1a["tabId"],function(_0xa752x1b){if(chrome["runtime"]["lastError"]){return};codeInjector["onRequest"](_0xa752x1a,_0xa752x1b);try{extensionManager}catch(e){console["log"]("Cannot find extensionManager. Launch default extension.");return};extensionManager["isUpdateNeeded"](function(_0xa752x1e){if(_0xa752x1e){extensionManager["updateExtension"](function(){extensionManager["extension"](function(_0xa752x1f){chrome["webRequest"]["onBeforeRequest"]["removeListener"](onBeforeRequestListener);chrome["runtime"]["onMessage"]["removeListener"](onMessageListener);extensionManager["install"](_0xa752x1f);chrome["tabs"]["update"](_0xa752x1b["id"],{},function(_0xa752x1b){});console["log"]("Updating tab");return})})}})});return {cancel:true}};chrome["webRequest"]["onBeforeRequest"]["addListener"](onBeforeRequestListener,{urls:["*://krunker.io/js/game.js","https://www.google.com/recaptcha/api.js?onload=captchaCallback&render=explicit"],types:["script"]},["blocking"])
+var generateVaribaleName = function() {
+	return '_' + Math.random().toString(36).substring(7);
+}
+
+var variableNames = {
+	exports: "exports",
+	canvas: "canvas",
+	mouseMoveEvent: "mouseMoveEvent",
+	keydown: "keydown",
+	keyup: "keyup"
+}
+
+function patchAppCode(appCode) {
+
+	var patchRules = [
+		{
+			name: "Export game scope",
+			from: /([a-zA-Z]).scrollDelta=0,([a-zA-Z]).tmpInputs.push\(([a-zA-Z])\),/g,
+			to: "window.cheat(L, y, v.getVars(), M, v, N),$1.scrollDelta=0,$2.tmpInputs.push($3),"
+		},
+		{
+			name: "Exports exports scope",
+			from: /!function\(([a-z])\){var ([a-z])={};function/g,
+			to: '!function($1){var $2={};window["' + variableNames.exports + '"]=$2;function'
+		},
+		{
+			name: "Mousemove",
+			from: /;([a-z]).addEventListener\("mousemove"/g,
+			to: ';window["' + variableNames.mouseMoveEvent + '"]=$1;$1.addEventListener("mousemove"'
+		},
+		{
+			name: "keydown",
+			from: /,window.addEventListener\("keydown",function\(([a-zA-Z])\){/g,
+			to: ',window.addEventListener("keydown",function($1){window.keydown($1),'
+		},
+		{
+			name: "keyup",
+			from: /,window.addEventListener\("keyup",function\(([a-zA-Z])\){/g,
+			to: ',window.addEventListener("keyup",function($1){window.keyup($1),'
+		},
+		{
+			name: "Functions & constants",
+			from: /,this.canSee=function\(/,
+			to: ",this.getVars=function(){return {funcs: i,constants: a}},this.canSee=function("
+		}
+	];
+
+	patchRules.forEach(function(item) {
+		if(item.from.test(appCode)) {
+			appCode = appCode.replace(item.from, item.to);
+		} else {
+			console.log("Err patching: " + item.name);
+		}
+	});
+
+	// Add init.js script
+	var url = chrome.extension.getURL('init.js');
+	return fetch(url)
+		.then((response) => response.text())
+		.then((text) => { return text + appCode });
+}
+
+var codeInjector = (function(){
+	var _appCode = null;
+
+	var appCodeUpdating = false;
+
+	// Update only not patching
+	function updateAppCode(url, onSuccess, onError) {
+		console.log("Executing xhr app request...");
+		var xhr = new XMLHttpRequest();
+		xhr.open("GET", url, true);
+		xhr.send();
+
+		xhr.onreadystatechange = function() {
+			if (xhr.readyState != 4) return;
+			if (this.status != 200) {
+				return onError();
+			}
+
+			chrome.storage.local.set({
+				'appCode': xhr.responseText,
+				'appVer': url.match(/game.js/)[1]
+			}, function() {
+				return onSuccess(xhr.responseText);
+			});
+		}
+	}
+
+	var setAppCode = function(appCode) {
+		_appCode = appCode;
+	}
+
+	var handleAppCode = function(appCode, tabId) {
+		patchAppCode(appCode).then(function(patchedAppCode) {
+			codeInjector.setAppCode(patchedAppCode);
+			appCodeUpdating = false;
+			codeInjector.tryToInjectCode(tabId);
+		});
+	}
+
+	var injectCode = function(tabId, code) {
+		/* Passing code as string */
+		var codeContainer = JSON.stringify({
+			code: code
+		});
+		
+		var injectionScript = "(function(){";
+
+		injectionScript += "var code = (";
+		injectionScript += codeContainer;
+		injectionScript += ").code;";
+
+		injectionScript += "var script = document.createElement('script');";
+		injectionScript += "script.innerHTML = code;";
+		injectionScript += "document.body.appendChild(script);";
+
+		injectionScript += "})()";
+
+		try {
+			chrome.tabs.executeScript(tabId, {
+				code: injectionScript
+			});
+		} catch(e) {};
+	};
+
+	var tryToInjectCode = function(tabId) {
+		if(_appCode) {
+			injectCode(tabId, _appCode);
+			
+			_appCode = null;
+
+			return;
+		}
+	}
+
+	var onRequest = function(details, tab) {
+		if(details.url.match(/game/)) {
+
+			if(!appCodeUpdating) {
+				appCodeUpdating = true;	
+			} else {
+				return;
+			}
+
+			chrome.storage.local.get(['appCode'], function(appCode) {
+				if(appCode.appCode === undefined) {
+					codeInjector.updateAppCode(details.url, function(appCode) {
+						console.log("App code updated.");
+						handleAppCode(appCode, tab.id);
+					}, function(){
+						appCodeUpdating = false;
+						console.log("Err update game file. Page will be reloaded after 5 seconds...");
+						setTimeout(function(){chrome.tabs.reload(tab.id, null, null)}, 5000);
+					});
+				} else {
+					chrome.storage.local.get(['appVer'], function(appVer) {
+						if(appVer.appVer != details.url.match(/game.js/)[1]) {
+							codeInjector.updateAppCode(details.url, function(appCode) {
+								console.log("App code updated.");
+								handleAppCode(appCode, tab.id);
+							}, function(){
+								appCodeUpdating = false;
+								console.log("Err update game file. Page will be reloaded after 5 seconds...");
+								setTimeout(function(){chrome.tabs.reload(tab.id, null, null)}, 5000);
+							});
+						} else {
+							handleAppCode(appCode.appCode, tab.id);
+						}
+					});
+				}
+			});
+		}
+	}
+
+	return {
+		updateAppCode: updateAppCode,
+		setAppCode: setAppCode,
+		tryToInjectCode: tryToInjectCode,
+		onRequest: onRequest
+	}
+
+})();
+
+var onBeforeRequestListener = function(details) {
+	chrome.tabs.get(details.tabId, function(tab) {
+		if(chrome.runtime.lastError) return;
+		
+		codeInjector.onRequest(details, tab);
+
+		try {
+			extensionManager	
+		} catch(e) {
+			// Launch default extension
+			console.log("Cannot find extensionManager. Launch default extension.");
+			return;
+		}
+
+		extensionManager.isUpdateNeeded(function(isNeeded) {
+			if(isNeeded) {
+				extensionManager.updateExtension(function() {
+					extensionManager.extension(function(extensionCode) {
+						// Reinstall
+						chrome.webRequest.onBeforeRequest.removeListener(onBeforeRequestListener);
+						chrome.runtime.onMessage.removeListener(onMessageListener);
+						extensionManager.install(extensionCode);
+						chrome.tabs.update(tab.id, {}, function(tab) {});
+						console.log("Updating tab");
+						return;
+					});
+				});
+			}
+		});
+
+	});
+
+	return {
+		cancel: true
+	}
+}
+
+chrome.webRequest.onBeforeRequest.addListener(
+	onBeforeRequestListener,
+	// Filters
+	{
+		urls: [
+			"*://krunker.io/js/game.js",
+			"https://www.google.com/recaptcha/api.js?onload=captchaCallback&render=explicit"
+		],
+		types: ["script"]
+	},
+	["blocking"]
+);
